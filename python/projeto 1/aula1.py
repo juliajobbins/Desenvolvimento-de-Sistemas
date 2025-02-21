7dd7cca94c17901ba2db3156b3207fc1a2145a2d
Lia <146204711+juliajobbins@users.noreply.github.com>
2025-02-21 09:19:39 -0300

    Add files via upload

---
+++ python/projeto 1/aula1.py
@@ -0,0 +1,16 @@
+import random
+sorteio = random.randint (1, 10)
+print("### Jogo de adivinhação! ###")
+print("Adivinhe o número que eu estou pensando, de 1 a 10")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(input("Digite o seu chute: "))
+    if (chute == sorteio):
+        print("Parabéns, você acertou!")
+    elif (chute > sorteio):
+        print("Chute um número menor!")
+    elif (chute < sorteio):
+        print("Chute um número maior!")
+    tentativa = tentativa + 1
---
+++ python/projeto 1/while.py
@@ -0,0 +1,5 @@
+contador = 1
+while (contador <=10):
+    print("Olá", contador)
+    contador = contador + 1
+print ("Este print está fora do laço")
